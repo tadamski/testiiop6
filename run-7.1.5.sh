6551eae597ae16e52f3695b218eef88981a8d91c
Brad Maxwell <bmaxwell@redhat.com>
2019-01-08 11:39:58 -0600

    updated run-7.1.5 classpath

--- run-7.1.5.sh
+++ run-7.1.5.sh
@@ -3,12 +3,13 @@
 jbossHome=$1
 shift
 modules="${jbossHome}/modules/system/layers/base"
-
 picketbox=${modules}/org/picketbox/main/picketbox-5.0.3.Final-redhat-3.jar:${modules}/org/picketbox/main/picketbox-commons-1.0.0.final-redhat-5.jar
-orb=${modules}/javax/orb/api/main/openjdk-orb-8.0.8.Final-redhat-1.jar:${modules}/org/jboss/as/jacorb/main/wildfly-jacorb-7.1.5.GA-redhat-00002.jar
-
+orb=${modules}/javax/orb/api/main/openjdk-orb-8.0.8.Final-redhat-1.jar:${modules}/org/wildfly/iiop-openjdk/main/wildfly-iiop-openjdk-7.1.5.GA-redhat-00002.jar
 slf4j="${jbossHome}/modules/system/layers/base/org/slf4j/main/slf4j-api-1.7.2.redhat-4.jar:${jbossHome}/modules/system/layers/base/org/slf4j/impl/main/slf4j-jboss-logmanager-1.0.3.GA-redhat-1.jar"
 client="target/testiiop.jar:${jbossHome}/bin/client/jboss-cli-client.jar:${jbossHome}/bin/client/jboss-client.jar"
+
+#orb=${modules}/javax/orb/api/main/openjdk-orb-8.0.8.Final-redhat-1.jar:${modules}/org/jboss/as/jacorb/main/wildfly-jacorb-7.1.5.GA-redhat-00002.jar:${modules}/org/wildfly/iiop-openjdk/main/wildfly-iiop-openjdk-7.1.5.GA-redhat-00002.jar
+
 
 cp="${client}:${orb}:${slf4j}:${picketbox}"
 
